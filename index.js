ffae8943a66fddc8c7200525ea222cd6a36d7127
Atul Kumar Pandey <pandeyatul0095@gmail.com>
2022-01-28 19:49:08 +0530

    UI updated
    Added three buttons, included divs for displaying results and points.

--- index.js
+++ index.js
@@ -12,49 +12,105 @@ function computerPlay(random) {
 }
 let playerPoints = 0
 let computerPoints = 0
+let body = document.querySelector("body")
 
-let computerSelection = (computerPlay(random))
-let playerSelection = ""
 function playRound(playerSelection, computerSelection){
+    if((playerPoints + computerPoints) !== 5){
+    let result = document.createElement('div');
+    result.innerHTML = " "
     if(playerSelection === "rock" ){
        if(computerSelection === "paper") {
-        computerPoints +=1
-           return "You lose! Paper Beats Rock!!"
-       } else 
+            computerPoints +=1;
+            result.innerHTML = "You lose! Paper Beats Rock!!";
+            body.appendChild(result);
+            return result ;
+       } else if(computerSelection === "scissor")
        {
-        playerPoints +=1
-           return "You win! Rock Beats Scissor!!"
+            playerPoints +=1;
+            result.innerHTML = "You win! Rock Beats Scissor!!";
+            body.appendChild(result);
+            return result;
+       } else {
+            result.innerHTML = "It's a tie!!";
+            body.appendChild(result);
+            return result;
        }
     } else  if(playerSelection === "paper" ){
         if(computerSelection === "scissor") {
-            computerPoints +=1
-            return "You lose! Scissor Beats Paper!!"
-        } else 
+            computerPoints +=1;
+            result.textContent = "You lose! Scissor Beats Paper!!";
+            body.appendChild(result);
+            return result;
+           
+        } else if(computerSelection === "rock")
         {
-            playerPoints +=1
-            return "You win! Paper Beats Rock!!"
-        }
+            playerPoints +=1;
+            result.textContent = "You win! Paper Beats Rock!!";
+            body.appendChild(result);
+            return result;
+        } else {
+            result.textContent = "It's a tie!!";
+            body.appendChild(result);
+           return result;
+           }
      } else  if(playerSelection === "scissor" ) {
             if(computerSelection === "paper") {
-                playerPoints +=1
-                return "You win! Scissor Beats Paper!!"
-            } else 
+                playerPoints +=1;
+            result.textContent = "You win! Scissor Beats Paper!!";
+            body.appendChild(result);
+            return result;
+            } else if(computerSelection === "rock")
             {
-                computerPoints +=1
-                return "You lose! Rock Beats Scissor!!"
-            }       
-        } else return "Please Enter Rock , Paper Or Scissor Only!!"
+                computerPoints +=1;
+                result.textContent = "You lose! Rock Beats Scissor!!";
+            body.appendChild(result);
+            return result;
+            } else {
+                result.textContent = "It's a tie!!";
+                body.appendChild(result);
+               return result;
+               }       
+        } 
+    } 
 }
 
 function game() {
-    for(let i =0; i<5 ;i++) {
-         playerSelection = prompt("rock, paper, scissor")
-        playerSelection = playerSelection.toLowerCase();
-        console.log(playRound(playerSelection, computerSelection))
-        console.log("Player Points:",playerPoints, "Computer Points:", computerPoints)  
-    } 
-    if(playerPoints>computerPoints) {
-    console.log("Player Won")
-}       else console.log("Computer Won")      
-}
+    {
+        const rockSelection = document.querySelector("#rock");
+        const paperSelection = document.querySelector("#paper");
+        const scissorSelection = document.querySelector("#scissor");
+        let computerSelection = 0;
+        let playerSelection = " ";
+        let points = document.createElement('div');
+        rockSelection.addEventListener('click', e => 
+        {   
+            computerSelection = computerPlay(random);
+            playerSelection = e.target.id;
+            playRound(playerSelection, computerSelection);
+            points.innerHTML = `Player Points: ${playerPoints} Computer Points: ${computerPoints}` 
+            body.appendChild(points)
+            return points;
+            }
+            );
+        paperSelection.addEventListener('click', e => 
+            {
+            computerSelection = (computerPlay(random));
+            playerSelection = e.target.id;
+            playRound(playerSelection, computerSelection)
+            points.innerHTML = `Player Points: ${playerPoints} Computer Points: ${computerPoints}` 
+            body.appendChild(points)
+            return points;}
+            );  
+        scissorSelection.addEventListener('click', e => 
+            {
+            computerSelection = (computerPlay(random));
+            playerSelection = e.target.id;
+            playRound(playerSelection, computerSelection)
+            points.innerHTML = `Player Points: ${playerPoints} Computer Points: ${computerPoints}` 
+            body.appendChild(points)
+            return points;}
+            );  
+            
+        } 
+    }
 game()
